f53bd1a7db38b95e6d268d152b530f02f6de52f1
Gap6769 <90163336+Gap6769@users.noreply.github.com>
2021-10-23 18:08:12 -0300

    1

--- preguntaEntrevista/main.py
+++ preguntaEntrevista/main.py
@@ -4,5 +4,21 @@
 # ej ([{hello world}]) is valid, but ([{hello world]}]) is not valid
 # ej {[test]} is valid, but {[test)}] is not valid
 
+<<<<<<< Updated upstream
 def test():
     print("test")
+=======
+
+
+def checkString(string):
+    count = 0
+    openCaja = [
+    openParentesis = (
+    openCoso = {
+    closeCaja = ]
+    closeParentesis = )
+    closeCoso = }
+    close = ]})
+    for i in string:
+        if i in open:
+>>>>>>> Stashed changes
