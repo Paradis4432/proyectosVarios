190419696d3ffd47eaa88abc06410c9693dc6149
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-10-23 18:07:49 -0300

    update

--- preguntaEntrevista/main.py
+++ preguntaEntrevista/main.py
@@ -1,6 +1,8 @@
-#dada la string determinar si es valida
-#todo inicio debe tener un final
-#debe cerrar en el orden correcto
-#ej ([{hello world}]) is valid, but ([{hello world]}]) is not valid
-#ej {[test]} is valid, but {[test)}] is not valid
+# dada la string determinar si es valida
+# todo inicio debe tener un final
+# debe cerrar en el orden correcto
+# ej ([{hello world}]) is valid, but ([{hello world]}]) is not valid
+# ej {[test]} is valid, but {[test)}] is not valid
 
+def test():
+    print("test")
