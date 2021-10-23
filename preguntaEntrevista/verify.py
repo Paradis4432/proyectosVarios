9ebbb27f05ed070fa6d1ee5c7f9dec0ecf4b0ff8
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-10-23 19:02:06 -0300

    Update verify.py

---
+++ preguntaEntrevista/verify.py
@@ -0,0 +1,56 @@
+# create two lists, one with ( { [ and the other with ) ] }
+from typing import final
+
+openings = ['(', '{', '[']
+closings = [')', '}', ']']
+
+stringTrue = "(()[]{})"
+stringFalse = "((})[]{})"
+
+finalString = stringFalse
+
+
+#             ^   ^
+# stringTrue = "( [ ] { } )"
+#              ^         ^
+
+# al encontrar un (, encuentra el siguiente ) y lo saca de finalString
+
+def checkBase(s):
+    # check if there is an even amount of () [] {}
+    if s.count('(') == s.count(')'):
+        if s.count('[') == s.count(']'):
+            if s.count('{') == s.count('}'):
+                return True
+    return False
+
+
+def checkNext(s,finalString = finalString):
+    print(finalString)
+
+    for i in s:
+        if i in openings:
+            for x in s:
+                if x in closings:
+                    # remove i and x from finalString
+                    finalString = finalString.replace(i, "")
+                    finalString = finalString.replace(x, "")
+                    print("removing")
+                else:
+                    print("not removing")
+    
+    if len(finalString) == 0:
+        print("string is valid")
+    else:
+        print("string is not valid")
+
+
+def basicTest():
+    if checkBase(stringFalse):
+        print("True")
+    else:
+        print("False")
+        return
+    checkNext(stringFalse)
+
+basicTest()
