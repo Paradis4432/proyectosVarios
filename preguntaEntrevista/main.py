61c5f310a12170283526b63c609185ddaa169cc4
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-10-23 18:15:15 -0300

    Update main.py

--- preguntaEntrevista/main.py
+++ preguntaEntrevista/main.py
@@ -4,32 +4,3 @@
 # ej ([{hello world}]) is valid, but ([{hello world]}]) is not valid
 # ej {[test]} is valid, but {[test)}] is not valid
 
-<<<<<<< Updated upstream
-def test():
-    print("test")
-<<<<<<< Updated upstream
-=======
-
-
-def checkString(string):
-    count = 0
-    openCaja = [
-    openParentesis = (
-    openCoso = {
-    closeCaja = ]
-    closeParentesis = )
-    closeCoso = }
-    close = ]})
-    for i in string:
-        if i in open:
->>>>>>> Stashed changes
-=======
-
-def take(s):
-    x = []
-    for i in s:
-        x += i
-    print(x)
-
-take("test")
->>>>>>> Stashed changes
