a65436e0e701da5e517a5b343d5eff6fe97818ec
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-11-03 11:11:58 -0300

    Update enigmaMachine.py

--- random/enigmaMachine.py
+++ random/enigmaMachine.py
@@ -6,15 +6,3 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 posInt = alphabet.index(let)
 #make a list of the rotors
 rotors = [1,1,1]
-
-def convert(posInt = posInt, rotors = rotors):
-    for r in rotors:
-        posInt = posInt + r
-        if rotors[rotors.index(r)] == 26:
-            rotors[rotors.index(r)] = 1
-        else:
-            rotors[rotors.index(r)] = rotors[rotors.index(r)] + 1
-    return posInt
-
-for i in range(0,3):
-    print(alphabet[convert()])
