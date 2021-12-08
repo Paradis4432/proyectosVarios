89b3aab6d31b0a06478c754986b8efd3726f6e64
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-12-08 16:41:13 -0300

    AOC day 3/4

--- AOC day 3/main.py
+++ AOC day 3/main.py
@@ -32,3 +32,62 @@ decer = int(er,2)
 
 final = decgr * decer
 final
+
+#%%
+len(values[0])
+#%%
+# oxygen rate
+oxRate = 0
+co2Rate = 0
+fullList = values
+for bit in range(len(values[0])):
+
+    c0,c1 = 0,0
+    for x in fullList:
+        if x[bit] == "0":
+            c0 += 1
+        else:
+            c1 += 1
+    print("c0: ", c0)
+    print("c1: ", c1)
+
+    if c0 > c1:
+        fullList = [x for x in fullList if x[bit] == "0"]
+        print(fullList)
+    elif c1 >= c0:
+        fullList = [x for x in fullList if x[bit] == "1"]
+        print(fullList)
+    if len(fullList) == 1:
+        break
+print(fullList)
+oxRate = int(fullList[0],2)
+oxRate
+#%%
+#co2 rate
+fullList = values
+for bit in range(len(values[0])):
+
+    c0,c1 = 0,0
+    for x in fullList:
+        if x[bit] == "0":
+            c0 += 1
+        else:
+            c1 += 1
+    print("c0: ", c0)
+    print("c1: ", c1)
+
+    if c0 <= c1:
+        fullList = [x for x in fullList if x[bit] == "0"]
+        print(fullList)
+    elif c1 < c0:
+        fullList = [x for x in fullList if x[bit] == "1"]
+        print(fullList)
+    if len(fullList) == 1:
+        break
+print(fullList)
+co2Rate = int(fullList[0],2)
+co2Rate
+
+# %%
+print(oxRate, co2Rate)
+print(int(oxRate) * int(co2Rate))
---
+++ AOC day 4/data.py
@@ -0,0 +1,4 @@
+#%%
+with open("input.txt") as f:
+    data = f.readlines()
+    print(data)
