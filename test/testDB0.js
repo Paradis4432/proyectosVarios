e143ba6bbb2d3a58ad2ee7aeddfe578ab503a5ba
Paradis <Lucasezequiel120202@gmail.com>
2022-01-16 23:27:17 -0300

    update

---
+++ test/testDB0.js
@@ -0,0 +1,15 @@
+const db = require('better-sqlite3')('data.db');
+const fs = require('fs');
+
+db.prepare("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)").run();
+
+//db.prepare("INSERT INTO test (name, age) VALUES (@name, @age)").run({name: 'John', age: 42});
+//db.prepare("INSERT INTO test (name, age) VALUES (@name, @age)").run({name: 'Jane', age: 37});
+
+//db.prepare("DELETE FROM test WHERE id = @id").run({id: 1});
+
+db.prepare("UPDATE test SET name = @name, age = @age WHERE id = @id").run({id: 5, name: 'lucas', age: 25});
+
+db.prepare("SELECT * FROM test").all().forEach(row => {
+  console.log(row.id, row.name, row.age);
+});
